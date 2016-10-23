f1ed837a00acdacca07ba8af2184a8f68239c691
apanda <w_0482@qq.com>
2016-10-23 17:26:41 +0800

    gank.io的练习demo

--- app/src/main/java/com/apanda/base/Module/Gank/activity/model/PostArticleModel.java
+++
@@ -1,9 +0,0 @@
-package com.apanda.base.Module.Gank.activity.model;
-
-
-import com.apanda.base.Module.Gank.activity.contract.PostArticleContract;
-
-
-public class PostArticleModel implements PostArticleContract.Model {
-
-}
--- app/src/main/java/com/apanda/base/Module/Gank/activity/presenter/PostArticlePresenter.java
+++
@@ -1,8 +0,0 @@
-package com.apanda.base.Module.Gank.activity.presenter;
-
-
-import com.apanda.base.Module.Gank.activity.contract.PostArticleContract;
-
-public class PostArticlePresenter extends PostArticleContract.Presenter {
-
-}
